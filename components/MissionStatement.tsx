947e633ff18189d2237252153316f54f83d633b8
Dev_Srijani <srijani.bluehorse@mail.com>
2026-05-20 12:53:41 +0530

    changes

--- components/MissionStatement.tsx
+++ components/MissionStatement.tsx
@@ -1,6 +1,6 @@
 export default function MissionStatement() {
   return (
-    <div className="center-body center-styl top-gap">
+    <div className="center-body center-styl top-gap top-gap">
       <h1>
         <a href="#">
           MISSIO<span>N STA</span>TEMENT
